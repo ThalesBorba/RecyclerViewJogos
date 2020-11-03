58ed78f91875965befeba910f77d02da9482f7d3
thales <thales_borba@hotmail.com>
2020-11-03 14:34:08 -0300

    update files

---
+++ app/src/main/java/com/example/recyclerview/uiActivity/MainActivity.java
@@ -0,0 +1,65 @@
+package com.example.recyclerview.uiActivity;
+
+import android.os.Bundle;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.recyclerview.R;
+import com.example.recyclerview.modelos.Jogo;
+import com.example.recyclerview.utilitarios.JogoResponse;
+import com.example.recyclerview.utilitarios.JogoAdapter;
+import com.example.recyclerview.utilitarios.RetrofitCall;
+
+import java.util.ArrayList;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class MainActivity extends AppCompatActivity {
+
+    private JogoAdapter jogosAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recyclerview);
+
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl(RetrofitCall.urlBase)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+
+        RetrofitCall chamada = retrofit.create(RetrofitCall.class);
+        Call<JogoResponse> requestDeJogos = chamada.getJogos();
+        RecyclerView jogosRecyclerView = findViewById(R.id.lista_notas_recyclerview);
+
+
+        requestDeJogos.enqueue(new Callback<JogoResponse>() {
+            @Override
+            public void onResponse(Call<JogoResponse> call, Response<JogoResponse> response) {
+                if(!response.isSuccessful()) {
+                    Toast.makeText(getApplicationContext(), response.code(),
+                            Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                JogoResponse jogoResponse = response.body();
+                ArrayList<Jogo> jogos = jogoResponse.getJogos();
+                JogoAdapter jogosAdapter = new
+                        JogoAdapter(MainActivity.this, jogos);
+                jogosRecyclerView.setAdapter(jogosAdapter);
+
+            }
+
+            @Override
+            public void onFailure(Call<JogoResponse> call, Throwable t) {
+                Toast.makeText(getApplicationContext(), t.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/recyclerview/utilitarios/JogoAdapter.java
@@ -0,0 +1,78 @@
+package com.example.recyclerview.utilitarios;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.recyclerview.R;
+import com.example.recyclerview.modelos.Jogo;
+import com.squareup.picasso.Picasso;
+
+import java.util.ArrayList;
+
+public class JogoAdapter extends RecyclerView.Adapter<JogoAdapter.JogoViewHolder> {
+
+        private Context jogosContext;
+        private ArrayList<Jogo> jogoArrayList;
+
+        public JogoAdapter(Context context, ArrayList<Jogo> movieArrayList) {
+            jogosContext = context;
+            jogoArrayList = movieArrayList;
+        }
+
+        @NonNull
+        @Override
+        public JogoViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+            View v = LayoutInflater.from(jogosContext).inflate(R.layout.activity_cardview,
+                    parent, false);
+            return new JogoViewHolder(v);
+
+        }
+
+        @Override
+        public void onBindViewHolder(@NonNull JogoViewHolder holder, int position) {
+            Jogo jogoAtual = jogoArrayList.get(position);
+
+            String logoMandantePath = jogoAtual.getMandante().getUrlDaImagem();
+            String logoVisitantePath = jogoAtual.getVisitante().getUrlDaImagem();
+            String dataHoraPath = jogoAtual.getData() + " " + jogoAtual.getLocal() + " " +
+                    jogoAtual.getHora();
+            String placarPath;
+            if (jogoAtual.getPlacar() == null) {
+                placarPath = " X ";
+            } else {
+                placarPath = jogoAtual.getPlacar().getConcatenado();
+            }
+            holder.dataHora.setText(dataHoraPath);
+            holder.placar.setText(placarPath);
+            Picasso.get().load(logoMandantePath).fit().centerInside().into(holder.logoMandante);
+            Picasso.get().load(logoVisitantePath).fit().centerInside().into(holder.logoVisitante);
+        }
+
+        @Override
+        public int getItemCount() {
+            return jogoArrayList.size();
+        }
+
+        public class JogoViewHolder extends RecyclerView.ViewHolder {
+
+            public ImageView logoMandante;
+            public ImageView logoVisitante;
+            public TextView dataHora;
+            public TextView placar;
+            public JogoViewHolder(@NonNull View itemView) {
+                super(itemView);
+
+                logoMandante = itemView.findViewById(R.id.logoMandante);
+                logoVisitante = itemView.findViewById(R.id.logoVisitante);
+                dataHora = itemView.findViewById(R.id.dataHora);
+                placar = itemView.findViewById(R.id.placar);
+            }
+        }
+    }
